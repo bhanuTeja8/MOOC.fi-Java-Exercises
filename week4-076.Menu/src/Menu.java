7c34761911fff53610ca3aa926f17f19e5a6ada0
Win 11 <bhanutejavittanala02@gmail.com>
2025-06-05 20:33:18 +0530

    week 4

--- week4-076.Menu/src/Menu.java
+++ week4-076.Menu/src/Menu.java
@@ -8,6 +8,19 @@ public class Menu {
     public Menu() {
         this.meals = new ArrayList<String>();
     }
+    public void addMeal(String meal){
+        if (!this.meals.contains(meal)){
+            this.meals.add(meal);
+        }
+    }
+    public void printMeals(){
+        for (String meal:meals
+             ) {
+            System.out.println(meal);
 
-    // add the methods here
+        }
+    }
+    public void clearMenu(){
+        this.meals.clear();
+    }
 }
--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -3,14 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        // Write test code here
-        // Remember to remove all the extra code when doing assignments  79.3 and 79.4
+        NumberStatistics sum = new NumberStatistics();
+        NumberStatistics even = new NumberStatistics();
+        NumberStatistics odd = new NumberStatistics();
 
-        // Define three NumberStatistics objects in your program:
-        // The first is used to track the sum of all given numbers.
-        // The second takes care of even numbers and the third the odd numbers.
+        System.out.println("Type numbers: ");
 
-        // The tests does not work if you do not create the objects in the correct order
+        while (true) {
+            int number = Integer.parseInt(reader.nextLine());
+            if (number == -1) {
+                break;
+            }
 
+            sum.addNumber(number);
+
+            if (number % 2 == 0) {
+                even.addNumber(number);
+            } else {
+                odd.addNumber(number);
+            }
+        }
+
+        System.out.println("sum: " + sum.sum());
+        System.out.println("sum of even: " + even.sum());
+        System.out.println("sum of odd: " + odd.sum());
     }
 }
